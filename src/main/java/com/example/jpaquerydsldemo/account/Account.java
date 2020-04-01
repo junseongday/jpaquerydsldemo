7c860252427ad62d030e7953d9d66db8476e098b
junseongday <junseongday@gmail.com>
2020-04-01 13:39:39 +0900

    Initial commit

---
+++ src/main/java/com/example/jpaquerydsldemo/account/Account.java
@@ -0,0 +1,49 @@
+package com.example.jpaquerydsldemo.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String userName;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
